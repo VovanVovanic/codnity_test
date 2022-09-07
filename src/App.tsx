ddcec21f382c49f802e2cb6f724e012339341cb8
Vladimir <vladimirplotnikov371@gmail.com>
2022-09-07 16:11:01 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Category from './pages/category';
+import Details from './pages/details';
+import Main from './pages/main';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <main >
+          {/* <Header setPageInfo={setPageInfo} /> */}
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/details" element={<Details/>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/redux/auth/types.ts
+++ src/redux/auth/types.ts
@@ -1,9 +1,9 @@
-
+import { initState } from './reducer'
 export const SET_AUTH = "SET_AUTH"
 export const SET_LOADING = "SET_LOADING"
 export const SET_MESSAGE = "SET_MESSAGE"
 
-import { initState } from './reducer'
+
 export type InitStateType  = typeof initState
 
 export interface IMessage {
